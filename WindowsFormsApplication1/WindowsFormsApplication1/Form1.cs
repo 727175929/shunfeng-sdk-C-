760f51c9248639a059510f6e277b384257c5ef28
727175929 <727175929@qq.com>
2017-11-30 08:32:50 +0800

    解决了月卡号的问题

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -18,9 +18,17 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        String accesstoken = "BAEF8CF266CE4691AB0EA0BB2B6E3706";
         public Form1()
         {
             InitializeComponent();
+            comboBox1.Text = "标准快递";
+            comboBox2.Text = "第三方付";
+            comboBox3.Text = "不需要上门取件";
+            comboBox4.Text = "申请";
+            comboBox5.Text = "生成";
+            
+            testgetAccessToken();   //初始化获得AccessToken
         }
         private void button1_Click(object sender, EventArgs e)
         {
@@ -29,32 +37,33 @@ namespace WindowsFormsApplication1
 
         public void test()
         {
-            string url = "https://open-sbox.sf-express.com/rest/v1.0/order/access_token/BAEF8CF266CE4691AB0EA0BB2B6E3706/sf_appid/00037521/sf_appkey/21662E074E84B37EB4DBA0F89F9803AA";
+            string url = "https://open-sbox.sf-express.com/rest/v1.0/order/access_token/" + accesstoken + "/sf_appid/00037521/sf_appkey/21662E074E84B37EB4DBA0F89F9803AA";
             MessageReq<OrderReqDto> req = new MessageReq<OrderReqDto>();
             HeadMessageReq req2 = new HeadMessageReq{
                 transType = 200,
-                transMessageId = "201409040916141688"
+                transMessageId = "OPEN20160701-011"
             };
             req.head = req2;
             OrderReqDto dto = new OrderReqDto
             {
-                orderId = "201409051418379991429499",
-                expressType = 2,
+                orderId = "OPEN20160701-011",
+                expressType = 1,
                 payMethod = 1,
                 needReturnTrackingNo = 0,
                 isDoCall = 1,
                 isGenBillNo = 1,
-                custId = "7552732920",
-                payArea = "755CQ",
-                sendStartTime = "2014-4-24 09:30:00",
+                isGenEletricPic = 1,
+                payArea = "",
+                custId = "7550010173",
+                sendStartTime = "2018-4-24 09:30:00",
                 remark = "易碎物品，小心轻放"
             };
             DeliverConsigneeInfoDto dto2 = new DeliverConsigneeInfoDto
             {
-                address = "上地",
-                city = "朝阳",
-                company = "京东",
-                contact = "李四",
+                address = "神罗科技公司",
+                city = "北京市",
+                company = "神罗科技",
+                contact = "李逍遥",
                 country = "中国",
                 province = "北京",
                 shipperCode = "787564",
@@ -67,7 +76,7 @@ namespace WindowsFormsApplication1
                 city = "深圳",
                 company = "顺丰",
                 contact = "张三",
-                country = "中国",
+                country = "南山区",
                 province = "广东",
                 shipperCode = "518100",
                 tel = "0755-33915561",
@@ -84,16 +93,16 @@ namespace WindowsFormsApplication1
                 cargoTotalWeight = 12
             };
             List<AddedServiceDto> list = new List<AddedServiceDto>();
-            AddedServiceDto item = new AddedServiceDto
+            /*AddedServiceDto item = new AddedServiceDto
             {
                 name = "COD",
                 value = "20000"
             };
-            list.Add(item);
+            list.Add(item);*/
             AddedServiceDto dot6 = new AddedServiceDto
-            {
+            {/*
                 name = "CUSTID",
-                value = "7552732920"
+                value = "7552732920"*/
             };
             list.Add(dot6);
             dto.deliverInfo = dto2;
@@ -102,30 +111,38 @@ namespace WindowsFormsApplication1
             dto.addedServices = list;
             req.body = dto;
             MessageResp<OrderRespDto> resp = OrderTools.order(url, req);
+            string newLine = "信息:" + resp.head.message + "\n";
+            richTextBox1.Text = richTextBox1.Text.Insert(0, newLine);
+            newLine = null; 
         }    //模拟下单
 
         public void testOrderQuery()
         {
-            string url = "https://open-sbox.sf-express.com/rest/v1.0/order/query/access_token/6ECFF3C074A239BC28EF97D9627229B9/sf_appid/00037521/sf_appkey/21662E074E84B37EB4DBA0F89F9803AA";
+            string url = "https://open-sbox.sf-express.com/rest/v1.0/order/query/access_token/"+accesstoken+"/sf_appid/00037521/sf_appkey/21662E074E84B37EB4DBA0F89F9803AA";
             MessageReq<OrderQueryReqDto> req = new MessageReq<OrderQueryReqDto>();
             HeadMessageReq req2 = new HeadMessageReq
             {
                 transType = 0xcb,
-                transMessageId = "201409040916141689"
+                transMessageId = "201711290916141682"
             };
             req.head = req2;
             OrderQueryReqDto dto = new OrderQueryReqDto
             {
-                orderId = "201409051418379991429499"
+                orderId = "OPEN20160701-011"
             };
             req.body = dto;
             MessageResp<OrderQueryRespDto> resp = OrderTools.orderQuery(url, req);
-            richTextBox1.AppendText("message:" + resp.head.message + "\n");
-            richTextBox1.AppendText("transMessageId(流水号):" + resp.head.transMessageId + "\n");
-            richTextBox1.AppendText("transType(处理类型):" + resp.head.transType + "\n");
-            richTextBox1.AppendText("code（状态码）:" + resp.head.code + "\n");
-            richTextBox1.AppendText("resp.body:" + resp.body + "\n");
-        }
+            string newLine = "message:" + resp.head.message + "\n";
+            richTextBox1.Text = richTextBox1.Text.Insert(0, newLine);
+            newLine = "transMessageId(流水号):" + resp.head.transMessageId + "\n";
+            richTextBox1.Text = richTextBox1.Text.Insert(0, newLine);
+            newLine = "transType(处理类型):" + resp.head.transType + "\n";
+            richTextBox1.Text = richTextBox1.Text.Insert(0, newLine);
+            newLine = "code（状态码）:" + resp.head.code + "\n";
+            richTextBox1.Text = richTextBox1.Text.Insert(0, newLine);
+            newLine = "resp.body:" + resp.body + "\n";
+            richTextBox1.Text = richTextBox1.Text.Insert(0, newLine);
+        } //模拟查询
 
         public void testgetAccessToken()   //获得AccessToken
         {
@@ -142,10 +159,16 @@ namespace WindowsFormsApplication1
             };
             req.body = dto;*/
             MessageResp<TokenRespDto> resp = SecurityTools.applyAccessToken(url, req);
-            richTextBox1.AppendText("accessToken:" + resp.body.accessToken + "\n");
-            richTextBox1.AppendText("refreshToken:" + resp.body.refreshToken + "\n");
-            richTextBox1.AppendText("code:" + resp.head.code + "\n");
-            richTextBox1.AppendText("message:" + resp.head.message + "\n");
+            string newLine = "accessToken:" + resp.body.accessToken + "\n";
+            richTextBox1.Text = richTextBox1.Text.Insert(0, newLine);
+            newLine = "refreshToken:" + resp.body.refreshToken + "\n";
+            richTextBox1.Text = richTextBox1.Text.Insert(0, newLine);
+            newLine = "code:" + resp.head.code + "\n";
+            richTextBox1.Text = richTextBox1.Text.Insert(0, newLine);
+            newLine = "message:" + resp.head.message + "\n";
+            richTextBox1.Text = richTextBox1.Text.Insert(0, newLine);
+            newLine = null;   //清空newLine
+            accesstoken = resp.body.accessToken;
 
         }
         private void button2_Click(object sender, EventArgs e)
@@ -156,7 +179,54 @@ namespace WindowsFormsApplication1
         private void button3_Click(object sender, EventArgs e)
         {
             testgetAccessToken();
-        }   
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+         {
+            if ((e.KeyChar >= 'a' && e.KeyChar <= 'z') || (e.KeyChar >= 'A' && e.KeyChar <= 'Z') || (e.KeyChar >= '0' && e.KeyChar <= '9') || (e.KeyChar == 8)) 
+         {
+             e.Handled = false;
+         } 
+         else 
+         { 
+             e.Handled = true;
+         }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
  
